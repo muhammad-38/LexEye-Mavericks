c3406fdbe5573cf4c487a52fb5fc50ac143c3189
faseehullah khan <143412443+faseehullahkhan@users.noreply.github.com>
2025-09-11 20:11:25 +0500

    added import statements in the law and categories page

--- Frontend/src/pages/Catagories.jsx
+++ Frontend/src/pages/Catagories.jsx
@@ -1,71 +1,95 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 
+// Import images
+import Logo from "../assets/Actual-Logo.png";
+import TrafficPolice from "../assets/Traffic-Police.jpg";
+import Crime from "../assets/Crime.jpg";
+import Protest from "../assets/Protest.jpg";
+import Narcotics from "../assets/narcotics.webp";
+import Property from "../assets/property.web.webp";
+import Labour from "../assets/labour.webp";
+import Cyber from "../assets/cyber.webp";
+import Family from "../assets/family.webp";
+
+
+
 const Catagories = () => {
   return (
     <>
-    <div id="traffic">
+      <div id="traffic">
         <div id="traffic-heading">
-          <a href="./index.html"><img src="./images/Actual-Logo.png" alt="" class="logo" /></a>
-            <h1>Categories</h1>
-            <Link id="goback-btn" to='/'>Go Back</Link>
+          <a href="./index.html">
+            <img src={Logo} alt="Logo" className="logo" />
+          </a>
+          <h1>Categories</h1>
+          <Link id="goback-btn" to='/'>Go Back</Link>
         </div>
+
         <div>
-            <input type="text" placeholder="Search for categories..." id="search-bar"/>
+          <input type="text" placeholder="Search for categories..." id="search-bar" />
         </div>
+
         <div id="traffic-content">
-            <div class="categories">
-                <a href="./traffic.html">
-                    <img src="./images/Traffic-Police.jpg" alt="" />
-                    <div class="categories-name"><h3>TRAFFIC LAWS</h3></div>
-                </a> 
-            </div>
-            <div class="categories">
-                <a href="./traffic.html">
-                    <img src="./images/Crime.jpg" alt="" />
-                    <div class="categories-name"><h3>CRIMINAL LAWS</h3></div>
-                </a>
-            </div>
-            <div class="categories">
-                <a href="./traffic.html">
-                    <img src="./images/Protest.jpg" alt="" />
-                    <div class="categories-name"><h3>HUMAN RIGHTS</h3></div>
-                </a>
-            </div>
-            <div class="categories">
-                <a href="./traffic.html">
-                    <img src="./images/narcotics.webp" alt="" />
-                    <div class="categories-name"><h3>NARCOTICS LAWS</h3></div>
-                </a>
-            </div>
-            <div class="categories">
-                <a href="./traffic.html">
-                    <img src="./images/property.web.webp" alt="" />
-                    <div class="categories-name"><h3>PROPERTY LAWS</h3></div>
-                </a>
-            </div>
-            <div class="categories">
-                <a href="./traffic.html">
-                    <img src="./images/labour.webp" alt="" />
-                    <div class="categories-name"><h3>LABOUR LAWS</h3></div>
-                </a>
-            </div>
-            <div class="categories">
-                <a href="./traffic.html">
-                    <img src="./images/cyber.webp" alt="" />
-                    <div class="categories-name"><h3>CYBER LAWS</h3></div>
-                </a>
-            </div>
-            <div class="categories">
-                <a href="./traffic.html">
-                    <img src="./images/family.webp" alt="" />
-                    <div class="categories-name"><h3>FAMILY LAWS</h3></div>
-                </a>
-            </div>
+          <div className="categories">
+            <a href="./traffic.html">
+              <img src={TrafficPolice} alt="Traffic Police" />
+              <div className="categories-name"><h3>TRAFFIC LAWS</h3></div>
+            </a>
+          </div>
+
+          <div className="categories">
+            <a href="./traffic.html">
+              <img src={Crime} alt="Crime" />
+              <div className="categories-name"><h3>CRIMINAL LAWS</h3></div>
+            </a>
+          </div>
+
+          <div className="categories">
+            <a href="./traffic.html">
+              <img src={Protest} alt="Protest" />
+              <div className="categories-name"><h3>HUMAN RIGHTS</h3></div>
+            </a>
+          </div>
+
+          <div className="categories">
+            <a href="./traffic.html">
+              <img src={Narcotics} alt="Narcotics" />
+              <div className="categories-name"><h3>NARCOTICS LAWS</h3></div>
+            </a>
+          </div>
+
+          <div className="categories">
+            <a href="./traffic.html">
+              <img src={Property} alt="Property" />
+              <div className="categories-name"><h3>PROPERTY LAWS</h3></div>
+            </a>
+          </div>
+
+          <div className="categories">
+            <a href="./traffic.html">
+              <img src={Labour} alt="Labour" />
+              <div className="categories-name"><h3>LABOUR LAWS</h3></div>
+            </a>
+          </div>
+
+          <div className="categories">
+            <a href="./traffic.html">
+              <img src={Cyber} alt="Cyber" />
+              <div className="categories-name"><h3>CYBER LAWS</h3></div>
+            </a>
+          </div>
+
+          <div className="categories">
+            <a href="./traffic.html">
+              <img src={Family} alt="Family" />
+              <div className="categories-name"><h3>FAMILY LAWS</h3></div>
+            </a>
+          </div>
         </div>
-    </div>
+      </div>
     </>
   )
 }
 
-export default Catagories
+export default Catagories;
--- Frontend/src/pages/Laws.jsx
+++ Frontend/src/pages/Laws.jsx
@@ -1,52 +1,86 @@
-import React from 'react'
+import React from "react";
+import Logo from "../assets/Actual-Logo.png";
 
 const Laws = () => {
   return (
     <>
-    <div id="traffic">
+      <div id="traffic">
         <div id="traffic-heading">
-          <a href="./index.html"><img src="./images/Actual-Logo.png" alt="" class="logo" /></a>
-            <h1>Traffic</h1>
-            <a id="goback-btn" href="./categories.html">Categories</a>
+          <a href="./index.html">
+            <img src={Logo} alt="Logo" className="logo" />
+          </a>
+          <h1>Traffic</h1>
+          <a id="goback-btn" href="./categories.html">
+            Categories
+          </a>
         </div>
-        <div>
-            <input type="text" placeholder="Search for laws..." id="search-bar"/>
-        </div>
-        <div id="traffic-content">
-            <div class="content-box">
-                <span><h1>Hello</h1><i class="fa-solid fa-circle-down"></i></span>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-                <button>Read More</button>
-            </div>
-            <div class="content-box">
-                <span><h1>Hello</h1><i class="fa-solid fa-circle-down"></i></span>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-                <button>Read More</button>
-            </div>
-            <div class="content-box">
-                <span><h1>Hello</h1><i class="fa-solid fa-circle-down"></i></span>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-                <button>Read More</button>
-            </div>
-            <div class="content-box">
-                <span><h1>Hello</h1><i class="fa-solid fa-circle-down"></i></span>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-                <button>Read More</button>
-            </div>
-            <div class="content-box">
-                <span><h1>Hello</h1><i class="fa-solid fa-circle-down"></i></span>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-                <button>Read More</button>
-            </div>
-            <div class="content-box">
-                <span><h1>Hello</h1><i class="fa-solid fa-circle-down"></i></span>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-                <button>Read More</button>
-            </div>
-        </div>
-    </div>
-    </>
-  )
-}
 
-export default Laws
+        <div>
+          <input
+            type="text"
+            placeholder="Search for laws..."
+            id="search-bar"
+          />
+        </div>
+
+        <div id="traffic-content">
+          <div className="content-box">
+            <span>
+              <h1>Hello</h1>
+              <i className="fa-solid fa-circle-down"></i>
+            </span>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <button>Read More</button>
+          </div>
+
+          <div className="content-box">
+            <span>
+              <h1>Hello</h1>
+              <i className="fa-solid fa-circle-down"></i>
+            </span>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <button>Read More</button>
+          </div>
+
+          <div className="content-box">
+            <span>
+              <h1>Hello</h1>
+              <i className="fa-solid fa-circle-down"></i>
+            </span>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <button>Read More</button>
+          </div>
+
+          <div className="content-box">
+            <span>
+              <h1>Hello</h1>
+              <i className="fa-solid fa-circle-down"></i>
+            </span>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <button>Read More</button>
+          </div>
+
+          <div className="content-box">
+            <span>
+              <h1>Hello</h1>
+              <i className="fa-solid fa-circle-down"></i>
+            </span>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <button>Read More</button>
+          </div>
+
+          <div className="content-box">
+            <span>
+              <h1>Hello</h1>
+              <i className="fa-solid fa-circle-down"></i>
+            </span>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <button>Read More</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Laws;
